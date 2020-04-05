272eee51d77899befb80d4026912d9af07419baf
HendricksK <kurvinhendricks@gmail.com>
2020-04-05 18:51:18 +0200

    short decleration operator

--- godeclreation/pkg_intro.go
+++ godeclreation/pkg_intro.go
@@ -5,9 +5,15 @@ import (
 )
 
 func main() {
+	x := 42 // Declares variable x and assigns a value
 	n, err := fmt.Println("Hello, playground", 239489234, true)
 	if err != nil { // Checking for nill
 		fmt.Println(err)
 	}
-	fmt.Println(n)
+	fmt.Println("value of n", n)
+	fmt.Println("value of x", x)
+	x = 99
+	fmt.Println("value of x", x)
+	y := 100 + 34234 // This is a statement
+	fmt.Println("value of y y := 100 + 34234", y)
 }
