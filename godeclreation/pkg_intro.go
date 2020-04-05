1f5c76815db3095ef924db81f30c62ed9d57e03e
HendricksK <kurvinhendricks@gmail.com>
2020-04-05 19:37:09 +0200

    EOD commit

--- godeclreation/pkg_intro.go
+++ godeclreation/pkg_intro.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+var y = 43 // Declaring with var allowed outside of body. Scope
+var z int  // Declreation of var z of type int, but no value assigned
+// Each element of such a variable or value is set to the zero value for its type: false for booleans, 0 for numeric types, "" for strings, and nil for pointers, functions, interfaces, slices, channels, and maps https://golang.org/ref/spec#The_zero_value
+
 func main() {
 	x := 42 // Declares variable x and assigns a value
 	n, err := fmt.Println("Hello, playground", 239489234, true)
@@ -14,6 +18,14 @@ func main() {
 	fmt.Println("value of x", x)
 	x = 99
 	fmt.Println("value of x", x)
-	y := 100 + 34234 // This is a statement
-	fmt.Println("value of y y := 100 + 34234", y)
+
+	foo()
+}
+
+func foo() {
+	// Declaring a varibale and assign the value using the short declreation operator, operating on opperands
+	// Declaring of a certain type
+	// Declaring of var outside of function body allows us to use variables that have been declared outside of function body
+	fmt.Println("Variable y decalred as var", y)
+	fmt.Println("Declreation of z with no value but type int = ", z)
 }
